4dc60659ea499e8ae2f6c23fc0088cc95b5b4c1d
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-06-02 12:08:46 +0530

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require('express');
+const path = require('insex.html');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
