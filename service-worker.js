46469a9c6bf9e8104c4e88646aa66e084673b951
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-06-02 11:53:44 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,39 @@
-// service-worker.js
-// service-worker.js
-const CACHE_NAME = 'simple-pwa-cache-v1';
+const CACHE_NAME = 'qr-code-generator-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/manifest.json',
-    '/icon-192x192.png',
-    '/icon-512x512.png'
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/qrcode.min.js',
+  '/bootstrap.min.css',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                return response || fetch(event.request);
-            })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
